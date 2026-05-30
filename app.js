b1d078dfa43524e2aa847ca3d848e8b1361a10e4
Emojiojio <150666339+Emojiojio@users.noreply.github.com>
2026-05-30 10:11:44 -0700

    Auto deploy update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-import { cities, vegetables, farmingModels, pestControls, fertilizers, regions, categories } from './data.js?v=1780160558462';
-import { weatherData } from './weather_data.js?v=1780160558462';
+import { cities, vegetables, farmingModels, pestControls, fertilizers, regions, categories } from './data.js?v=1780161104243';
+import { weatherData } from './weather_data.js?v=1780161104243';
 import { auth, googleProvider, signInWithPopup, signOut, onAuthStateChanged, db, doc, setDoc, getDoc, onSnapshot, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from './firebase-config.js';
 
 let currentUser = null;
